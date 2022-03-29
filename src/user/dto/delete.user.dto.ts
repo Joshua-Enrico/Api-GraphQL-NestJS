fb3898ac7830d86c7f61a37a0253eb85661e6cac
Joshua-Enrico <joshuaenrico123@gmail.com>
2022-03-29 02:18:37 -0500

    new dtos

--- src/user/dto/delete.user.dto.ts
+++ src/user/dto/delete.user.dto.ts
@@ -2,14 +2,8 @@ import { Field, InputType } from "@nestjs/graphql";
 import { IsNotEmpty } from "class-validator";
 
 @InputType()
-export class CreateUserDto {
+export class DeleteUserDto {
     @IsNotEmpty()
     @Field({ nullable: true })
-    name: string;   
-
-    @Field()
-    email: string;
-
-    @Field()
-    password: string;
+    id: string;
 }
