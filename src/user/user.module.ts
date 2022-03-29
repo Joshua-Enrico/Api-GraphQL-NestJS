b09b578c7a6f5ceb2cd5f832d52cf2181ebc6910
Joshua-Enrico <joshuaenrico123@gmail.com>
2022-03-29 02:20:08 -0500

    adding common modules

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 import { PrismaModule } from 'src/core/prisma/prisma.module';
-import { EncryptionsModule } from 'src/commons/encryptions/encryptions.module';
+import { CommonsModule } from 'src/commons/commons.module';
 
 @Module({
-  imports: [PrismaModule, EncryptionsModule],
+  imports: [PrismaModule, CommonsModule],
   providers: [UserService, UserResolver]
 }) 
 export class UserModule {}
