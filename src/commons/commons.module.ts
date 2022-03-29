20aa1189ae3a7343b4e75b3884e6536a611cbe85
Joshua-Enrico <joshuaenrico123@gmail.com>
2022-03-29 02:18:07 -0500

    global modules

--- src/commons/commons.module.ts
+++ src/commons/commons.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EncryptionsModule } from './encryptions/encryptions.module';
+import { AuthJwtModule } from './auth-jwt/auth-jwt.module';
+import { GuardsModule } from './guards/guards.module';
 
 @Module({
-  imports: [EncryptionsModule],
-  exports: [EncryptionsModule]
+  imports: [EncryptionsModule, AuthJwtModule, GuardsModule],
+  exports: [EncryptionsModule, GuardsModule, AuthJwtModule],
 })
 export class CommonsModule {}
